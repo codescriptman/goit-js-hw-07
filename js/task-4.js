641e3480ff742c250e9e35507c46e92d0f5baf4a
Azazello <donazazello1312@gmail.com>
2025-07-21 17:16:57 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,24 @@
 const form = document.querySelector(".login-form");
-form.addEventListener("submit", (event) => {
+const email = document.querySelector('input[type="email"]');
+const password = document.querySelector('input[type="password"]');
+
+const elements = {};
+form.addEventListener("submit", handle);
+
+function handle(event) {
   event.preventDefault();
-  if (condition) {
+  let obj = {
+    email: null,
+    password: null,
+  };
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
   }
-});
+  obj.email = email.trim();
+  obj.password = password.trim();
+  console.log(obj);
+  form.reset();
+}
